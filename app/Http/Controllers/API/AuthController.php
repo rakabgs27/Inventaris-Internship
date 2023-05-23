2c3b61294f977464d0db65ccf6df56d78a80a3d0
rakabgs27 <rakabgs2000@gmail.com>
2023-05-24 01:06:40 +0700

    cleancode login api

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -4,50 +4,32 @@ namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Resources\UserResource;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    /**
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function login(Request $request)
+    public function login(LoginRequest $request)
     {
-        // Validasi inputan email dan password
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
-
-        // Jika validasi gagal, kirim respon error
-        if ($validator->fails()) {
-            return response()->json([
-                'status' => false,
-                'data' => null,
-                'message' => $validator->errors()->first(),
-            ], 400);
-        }
-
-        // Mencoba melakukan login
+        $messageBerhasil = 'Login berhasil';
+        $messageGagal = 'Login gagal. Email atau password salah';
         if (Auth::attempt($request->only('email', 'password'))) {
-            // Jika login berhasil, buat token
             $user = Auth::user();
             $token = $user->createToken('API Token')->plainTextToken;
 
             return response()->json([
                 'status' => true,
-                'data' => $token,
-                'message' => 'Login berhasil',
+                'data' => new UserResource($user),
+                'token' => $token,
+                'message' => $messageBerhasil,
             ], 200);
         }
 
-        // Jika login gagal, kirim respon error
         return response()->json([
             'status' => false,
             'data' => null,
-            'message' => 'Login gagal. Email atau password salah.',
+            'message' => $messageGagal,
         ], 401);
     }
 }
